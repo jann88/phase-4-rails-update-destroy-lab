0dd6afc8244dc57b8e58864d3bf92461ab85e03a
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-10-01 00:24:06 +0300

    commit tree 39b74fb2a753636ae0c685622f268cbcab1b3f4a

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,7 +17,29 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  #update plant
+  def update
+      plant = Plant.find_by(id: params[:id])
+      if plant 
+        plant.update(plant_params)
+        render json: plant, status: :accepted
+      else 
+        render json: { error: "Plant not found" }, status: :not_found
+      end
+  end
 
+  #delete plant
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant 
+      plant.destroy
+      head :no_content
+      #render json: []
+    else
+      render json: { error: "Plant nof found"}, status: :not_found
+    
+    end
+  end
   private
 
   def plant_params
